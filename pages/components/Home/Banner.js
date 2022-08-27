35d4653154ab72d8171a92346b06aec481863150
Yeasir-Hossain <yeasir06@gmail.com>
2022-08-27 14:28:04 +0600

    banner logo

--- pages/components/Home/Banner.js
+++ pages/components/Home/Banner.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import banner from '../../../public/valagents.png'
-import RDtext from '../../../public/Ravendise Png white 2.png'
+import RDtext from '../../../public/RavendiseBanner.png'
 
 const Banner = () => {
     return (
